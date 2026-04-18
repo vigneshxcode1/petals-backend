d9d365f68176834910ef87c5053bb96dfbf3633d
vigneshxcode1 <vigneshxcode@gmail.com>
2026-04-18 17:24:14 +0530

    april 18

--- controllers/Products.js
+++ controllers/Products.js
@@ -37,20 +37,34 @@ export const newProduct = async (req, res) => {
   };
   
 
+export const getproducts = async (req, res, next) => {
+  try {
+    const resultperpage = 10;
 
-export const getproducts=async(req,res,next)=>{
-    const resultperpage=100
- const apiFeatures = new apiFeature(productModel.find(),req.query).search().filter().paginate(resultperpage)
-    const product= await apiFeatures.query
-res.status(200).json({
-    success :true,
-    message:"success get all products",
-   count:product.length,
-   product
-})
-}
+    // Build query
+    const apiFeatures = new apiFeature(productModel.find(), req.query)
+      .search()
+      .filter()
+      .paginate(resultperpage);
 
+    const products = await apiFeatures.query;
 
+    res.status(200).json({
+      success: true,
+      message: "Products fetched successfully",
+      count: products.length,
+      products,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
 export const singleproduct = async (req, res, next) => {
     try {
         const product = await productModel.findById(req.params.id);
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,38 +1,40 @@
 class apiFeature {
   constructor(query, strquery) {
-    (this.query = query),
-     (this.strquery = strquery);
+    this.query = query;
+    this.strquery = strquery;
   }
 
   search() {
-    let keyword = this.strquery.keyword
+    const keyword = this.strquery.keyword
       ? {
-          name: {
-            $regex: this.strquery.keyword,
-            $options: "i",
-          },
-        }
+        name: {
+          $regex: this.strquery.keyword,
+          $options: "i",
+        },
+      }
       : {};
 
-    this.query.find({ ...keyword });
+    this.query = this.query.find({ ...keyword });
     return this;
   }
-
   filter() {
     const querystrcopy = { ...this.strquery };
-    const removefield = ["keyword", "limit", "page"];
-    removefield.forEach((fild) => delete querystrcopy[fild]);
-    let strquery = JSON.stringify(querystrcopy);
-    strquery = strquery.replace(/\b(gt|gte|lt|lte)/g, (match) => `$${match}`);
-    this.query.find(JSON.parse(strquery));
-    return this;
-  }
-  
 
-  paginate(resultperpage) {
-    const currentpage = Number(this.strquery.page);
-    const skip = resultperpage + currentpage - 1;
-    this.query.limit(resultperpage).skip(skip);
+    const removefield = ["keyword", "limit", "page"];
+    removefield.forEach((field) => delete querystrcopy[field]);
+
+    // ✅ Fix category (case-insensitive)
+    if (querystrcopy.category) {
+      querystrcopy.category = {
+        $regex: `^${querystrcopy.category}$`,
+        $options: "i",
+      };
+    }
+
+    let strquery = JSON.stringify(querystrcopy);
+    strquery = strquery.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(strquery));
     return this;
   }
 }
